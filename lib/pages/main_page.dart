be6149bd31ff95b62c99473542ce0d80780cf186
purveshxd <do.psd23@gmail.com>
2023-03-23 10:48:34 +0530

    added bottom navbar

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:feedforward/services/email_sign_in.dart';
 import 'package:feedforward/services/google_sign_in.dart';
 import 'package:feedforward/services/login_ui.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -31,22 +32,73 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          TextButton(
+              onPressed: () async {
+                try {
+                  // await auth.signOut();
+                  await auth.signOut();
+                  await GoogleSignInProvider(context: context).googleLogout();
+                } on FirebaseException catch (e) {
+                  final snackBarE =
+                      SnackBar(content: Text(e.message.toString()));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBarE);
+                }
+              },
+              child: const Text("Logout"))
+        ],
         title: const Text("Welcome!!"),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          try {
-            // await auth.signOut();
-            await GoogleSignInProvider(context: context).googleLogout();
-          } on FirebaseException catch (e) {
-            final snackBarE = SnackBar(content: Text(e.message.toString()));
-            ScaffoldMessenger.of(context).showSnackBar(snackBarE);
-          }
+        onPressed: () {
+          debugPrint("The Floating Button was Pressed");
         },
-        child: const Icon(Icons.logout),
+        child: const Icon(Icons.add_rounded),
       ),
-      body: const Center(
-        child: Text("Welcome to FeedForward"),
+      body: Center(
+          child: ListView(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(Icons.home_outlined),
+                ),
+                Column(
+                  // mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "Address",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text("5 Star Boys Hostel, GHRCE")
+                  ],
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const CircleAvatar(
+                    child: Icon(Icons.person_rounded),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {},
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_rounded), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_rounded), label: "Profile"),
+        ],
       ),
     );
   }
